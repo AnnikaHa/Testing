a9dc7818a077bcce47ca335a6de49192765393de
Annika <annika800@gmail.com>
2013-10-07 21:47:02 +0300

    Lisasin 5e

--- Testing/src/Test.java
+++ Testing/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Heihopsti-Lisaharu");
 		System.out.println("Test1");
+		System.out.println("1,2,3,4,5");
 		
 	}
 }
